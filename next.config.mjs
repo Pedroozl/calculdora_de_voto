e747c3635aefb928fa6e0388201214950969004e
Pedroozl <57012227+Pedroozl@users.noreply.github.com>
2024-03-13 08:30:14 -0300

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
-  experimental: {
-    missingSuspenseWithCSRBailout: false,
-  },
+  output: "export"
 }
 
 export default nextConfig
